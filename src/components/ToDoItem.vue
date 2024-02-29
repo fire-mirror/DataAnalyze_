5ce2cc5ee577afb0e1aa086c62e68ef96d7f17fc
mirror fire <2325505640@qq.com>
2024-02-29 08:05:48 +0800

    first commit

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <input type="checkbox" id="toodo-item" checked="false" />
+  <label for="todo-item">My Todo Item</label>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean }
+  }
+};
+</script>
+
+
+<style scoped>
+
+</style>
